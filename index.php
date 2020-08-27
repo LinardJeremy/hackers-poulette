b8ec707f700517add56a0c8e5c84b9f44d717af2
Linard Jeremy <linard.jeremy.5@gmail.com>
2020-08-27 14:21:40 +0200

    modification page index ajout message erreur bootstrap

--- index.php
+++ index.php
@@ -20,37 +20,42 @@
         </div>
      <section class="col-10 mx-auto mb-3 sectionForm">
        
-        <form id="form" method="post">
+        <form id="form" method="post" class="needs-validation" novalidate>
             <div class="form-group row col-12 mt-3">
                 <label for="lastnameInput" class="col-3">LastName :</label>
-                <input type="text" class="form-control col-3 text-center " name="lastname"id="lastnameInput" placeholder="Write here your lastname">
+                <input type="text" class="form-control col-3 text-center " required name="lastname"id="lastnameInput" placeholder="Write here your lastname">
+                <div class="invalid-feedback">Valeur manquante ou incorrect</div>
                 <label for="firstnameInput" class="col-3 text-center">Firstname :</label>
-                <input type="text" class="form-control col-3 text-center" name="firstname" id="firstnameInput" placeholder="Write here your firstname">
+                <input type="text" required class="form-control col-3 text-center" name="firstname" id="firstnameInput" placeholder="Write here your firstname">
+                <div class="invalid-feedback">Valeur manquante ou incorrect</div>
               </div>
             <div class="form-group">
               <label for="emailInput">Email address :</label>
-              <input type="email" name="mail" class="form-control" id="emailInput" placeholder="name@example.com">
+              <input type="email" required name="mail" class="form-control" id="emailInput" placeholder="name@example.com">
+              <div class="invalid-feedback">Valeur manquante ou incorrect</div>
             </div>
             <div class="form-group">
               <label for="genderSelect">Gender :</label>
-              <select class="form-control" name="gender" id="genderSelect">
+              <select class="form-control" name="gender" id="genderSelect" required>
                 <option value="Choose">Choose a gender.</option>
                 <option value="Man">Man</option>
                 <option value="Woman">Woman</option>
               
               </select>
+              <div class="invalid-feedback">Valeur manquante ou incorrect</div>
             </div>
             <div class="form-group">
               <label for="CountrySelect">Country</label>
-              <select multiple class="form-control" id="CountrySelect" name="country">
+              <select multiple class="form-control" id="CountrySelect" name="country" required>
                 <option value="Belgium">Belgium</option>
                 <option value="England">England</option>
                 <option value="France">France</option>
                 <option value="Germany">Germany</option>
                 <option value="Spain">Spain</option>
               </select>
+              <div class="invalid-feedback">Valeur manquante ou incorrect</div>
             </div>
-            <fieldset id="subjectCheck">
+            <fieldset id="subjectCheck" required>
             <legend>Choose the subject of your message :</legend>
             <div class="formCheck">
               <div class="custom-control custom-radio custom-control-inline ml-5 mt-4">
@@ -67,14 +72,15 @@
               </div>   
             </div>
             </fieldset>  
+            <div class="invalid-feedback">Valeur manquante ou incorrect</div>
           <label id="labelDefineOther" for="inputOtherText" class="labelOff">Define your subject here :</label>
           <input class="labelOff" name="subjectOther" type="text" id="inputOtherText">
             <!-- Text area -->
-            <div class="form-group">
+            <div class="form-group" required>
               <label for="message">Type your message here : </label>
-              <textarea name="message" class="form-control" id="message" rows="3"></textarea>
+              <textarea name="message" class="form-control" id="message" rows="3" required></textarea>
             </div>
-          
+            <div class="invalid-feedback">Valeur manquante ou incorrect</div>
             <button id="send" onclick="validateForm()" class="btn btn-info mb-3 buttonSend">Send</button>
 
           </form>
